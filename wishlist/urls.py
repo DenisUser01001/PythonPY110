222df6e1ee0056047e5a63dd905e740b95b53556
DenisUser01001 <dk_85@mail.ru>
2024-06-04 02:49:42 +0300

    Промежуточный этап -> скорректирован wishlist.html для отображения товаров из Избранного.

--- wishlist/urls.py
+++ wishlist/urls.py
@@ -8,4 +8,5 @@ app_name = 'wishlist'
 
 urlpatterns = [
         path('wishlist/', wishlist_view, name="wishlist_view"),
+        path('wishlist/remove/<str:id_product>', wishlilst_remove_view, name="remove_now"),
 ]
