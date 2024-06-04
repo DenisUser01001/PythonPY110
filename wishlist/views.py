4b7538d3da33f9ba66300ab414c8824f4774e55c
DenisUser01001 <dk_85@mail.ru>
2024-06-04 11:13:23 +0300

    Промежуточный этап -> в views Избранного добавлены функции обработки wishlist_add_json, wishlist_del_json, wishlist_json. В shop.html добавлены JS обработки нажатия кнопки "сердце" у продукта.

--- wishlist/views.py
+++ wishlist/views.py
@@ -1,9 +1,9 @@
 from django.contrib.auth import login, authenticate, logout
-from django.http import HttpRequest
+from django.http import HttpRequest, JsonResponse
 from django.shortcuts import render, redirect
 from django.contrib.auth import get_user
 
-from logic.services import view_in_wishlist
+from logic.services import view_in_wishlist, add_to_wishlist, remove_from_wishlist
 from store.models import DATABASE
 
 
@@ -20,3 +20,48 @@ def wishlist_view(request: HttpRequest):
                 products.append(product)
 
             return render(request, "wishlist/wishlist.html", context={"products": products})
+
+
+def wishlist_add_json(request, id_product: str):
+    """
+    Добавление продукта в избранное и возвращение информации об успехе или неудаче в JSON
+    """
+    if request.method == "GET":
+        result = add_to_wishlist(request, id_product)  # вызов обработчик из services.py добавляющий продукт в избранное
+        if result:
+            return JsonResponse({"answer": "Продукт успешно добавлен в избранное"},
+                                json_dumps_params={'ensure_ascii': False})  # JsonResponse с ключом "answer" и значением "Продукт успешно добавлен в избранное"
+
+        return JsonResponse({"answer": "Неудачное добавление в избранное"},
+                            status=404,
+                            json_dumps_params={'ensure_ascii': False})  # JsonResponse с ключом "answer" и значением "Неудачное добавление в избранное" и параметром status=404
+
+
+def wishlist_del_json(request, id_product: str):
+    """
+    Удаление продукта из избранного и возвращение информации об успехе или неудаче в JSON
+    """
+    if request.method == "GET":
+        result = remove_from_wishlist(request, id_product)  # обработчик из services.py удаляющий продукт из избранного
+        if result:
+            return JsonResponse({"answer": "Продукт успешно удалён из избранного"},
+                                json_dumps_params={'ensure_ascii': False})  # JsonResponse с ключом "answer" и значением "Продукт успешно удалён из избранного"
+
+        return JsonResponse({"answer": "Неудачное удаление из избранного"},
+                            json_dumps_params={'ensure_ascii': False})  # JsonResponse с ключом "answer" и значением "Неудачное удаление из избранного" и параметром status=404
+
+
+def wishlist_json(request):
+    """
+    Просмотр всех продуктов в избранном для пользователя и возвращение этого в JSON
+    """
+    if request.method == "GET":
+        current_user = get_user(request).username
+        data = view_in_wishlist(request)[current_user]  # данные о списке товаров в избранном у пользователя
+        if data:
+            return JsonResponse(data,
+                                json_dumps_params={'ensure_ascii': False, 'indent': 4})  # JsonResponse c data
+
+        return JsonResponse({"answer": "Пользователь не авторизирован"},
+                            status=404,
+                            json_dumps_params={'ensure_ascii': False})  # JsonResponse с ключом "answer" и значением "Пользователь не авторизирован" и параметром status=404
